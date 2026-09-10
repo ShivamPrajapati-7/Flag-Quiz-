636dd9943e4dd68e1ea1a42863fdc292df774004
Shivam <shivamprajapati2006@gmail.com>
2026-09-10 22:23:34 +0530

    database added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.navigation.safeargs)
 }
 
 android {
@@ -10,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shivaminteractive.flagquiz"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
@@ -40,9 +41,22 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.fragment)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
+    // Navigation dependencies
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+    androidTestImplementation(libs.androidx.navigation.testing)
+
+
+    // Kotlinx Serialization for Type-Safe Navigation
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/shivaminteractive/flagquiz/ResultFragment.kt
+++ app/src/main/java/com/shivaminteractive/flagquiz/ResultFragment.kt
@@ -21,7 +21,7 @@ class ResultFragment : Fragment() {
 
 
         }
-        
+
         fragmentResultBinding.exit.setOnClickListener {
 
 
