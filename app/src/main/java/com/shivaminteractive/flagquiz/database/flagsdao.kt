aa5de9247d4d0315d8abe08fcb0dff9ef8d84246
Shivam <shivamprajapati2006@gmail.com>
2026-09-12 23:59:34 +0530

    Add data access class

---
+++ app/src/main/java/com/shivaminteractive/flagquiz/database/flagsdao.kt
@@ -0,0 +1,24 @@
+package com.shivaminteractive.flagquiz.database
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.shivaminteractive.flagquiz.model.flagsmodel
+import com.techmania.flagquizwithsqlitedemo.DatabaseCopyHelper
+
+class flagsdao {
+    fun getrandomrecords(helper: DatabaseCopyHelper): ArrayList<flagsmodel>
+    {
+        val recordlist=ArrayList<flagsmodel>()
+        val db: SQLiteDatabase =helper.writableDatabase
+        val cursor: Cursor=db.rawQuery("select * from flags order by random() Limit 10",null)
+        val idindex=cursor.getColumnIndex("flag_id")
+        val nameindex=cursor.getColumnIndex("country_name")
+        val flagindex=cursor.getColumnIndex("flag_name")
+        while (cursor.moveToNext())
+        {
+            val recoard=flagsmodel(cursor.getInt(idindex),cursor.getString(nameindex),cursor.getString(flagindex)))
+            recordlist.add(recoard)
+        }
+        return recordlist
+    }
+}
---
+++ app/src/main/java/com/shivaminteractive/flagquiz/model/flagsmodel.kt
@@ -0,0 +1,8 @@
+package com.shivaminteractive.flagquiz.model
+
+data class flagsmodel(
+    val flag_id: Int,
+    val country_name: String,
+    val flag_name: String
+
+) 
